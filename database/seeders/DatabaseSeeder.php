616ce40c950a1a3371b7eb0ae407b2081999963e
Md Mahfoozur Rahman <tushersuvro@gmail.com>
2024-03-31 17:45:24 +0600

    schema migration and seeding of some genres and movies data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Genre::factory(10)->create()->each( function($cl) {
+            $cl->movies()->saveMany(
+                \App\Models\Movie::factory(rand(1,5))->make()
+            );
+        });
     }
 }
