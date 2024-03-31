ec5be1aa3eb6a0301051a8166e367c59fc71b058
Md Mahfoozur Rahman <tushersuvro@gmail.com>
2024-03-31 22:13:59 +0600

    Created movie create form with genre dropdown

---
+++ app/Http/Controllers/Api/GenreController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\GenreResource;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+    //
+    public function index() {
+        return GenreResource::collection(Genre::orderBy('id', 'desc')->get());
+    }
+}
